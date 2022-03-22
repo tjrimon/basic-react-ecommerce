624f8dcd4cf2faedd40e6c6c5012aefbaccf805a
tjrimonm <tjrimonr@gmail.com>
2022-03-23 00:47:56 +0600

    header section

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import logo from '../../images/logo.png'
+import './Header.css'
 
 const Header = () => {
     return (
-        <div>
-            <h1>This is header</h1>
-        </div>
+        <header className='header'>
+            <div className="container">
+                <nav className='nav'>
+                    <img src={logo} alt="" />
+                    <ul>
+                        <li><a href="/oder">Order</a></li>
+                        <li><a href="/review">Order Review</a></li>
+                        <li><a href="/inventory">Manage Inventory</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
     );
 };
 
